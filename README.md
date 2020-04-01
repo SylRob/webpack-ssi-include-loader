d5864f709aa257677c2e6e8ad88528a5b5cbe381
Robert, Sylvain <sylvain.robert@rakuten.com>
2020-04-01 13:30:33 +0900

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Webpack SSI Include Loader
+
+```js
+module: {
+      rules: [
+          ....
+          {
+            test: /\.html?$/,
+            use: [
+              {
+                loader: 'html-loader', // Used to output as html
+              },
+              {
+                loader: 'webpack-ssi-loader',
+                options: {
+                  localPath: path.join(__dirname, '/public'),
+                  location: 'https://carservice.rakuten.co.jp/',
+                },
+              },
+            ],
+          },
+          ...
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const loaderUtils = require("loader-utils");
+const assign = require("object-assign");
+const SSI = require("./lib/ssi");
+
+function getLoaderConfig(context) {
+	const query = loaderUtils.parseQuery(context.query);
+	const configKey = query.config || 'webpackSsiIncludeLoader';
+	const config = context.options &&
+              context.options.hasOwnProperty(configKey) ?
+              context.options[configKey] :
+              {};
+
+	delete query.config;
+
+	return assign(query, config);
+}
+
+module.exports = function (source) {
+  const config = getLoaderConfig(this);
+	const ssi = new SSI(config);
+
+	this.cacheable && this.cacheable();
+
+	return ssi(source);
+};
